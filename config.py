e7072faee1900f0f8affad05d2d434ab423757f1
tsaplia <yunnakheifets@gmail.com>
2023-03-26 00:15:49 +0100

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+country_id = 1
+consulate_id = 1
+service_id = 1
+
+delay = 60
+
+sound_name = "sound.mp3"
---
+++ flare_solverr.py
@@ -0,0 +1,38 @@
+import requests
+
+FS_URL = "http://localhost:8191/v1"
+headers = {
+    "Content-Type": "application/json",
+}
+
+
+def _request(data):
+    try:
+        resp = requests.post(FS_URL, headers=headers, data=data).json()
+    except Exception:
+        raise Exception("Couldn't connect to FlareSolver")
+    if resp['status'] != "ok":
+        raise Exception(resp['message'])
+    return resp
+
+
+class FSSession:
+    id = None
+
+    def __init__(self):
+        data = '{"cmd": "sessions.create"}'
+        self.id = _request(data)['session']
+
+    def __del__(self):
+        if self.id is None: return
+        data = f'{{"cmd": "sessions.destroy", "session": "{self.id}"}}'
+        _request(data)
+
+    def get(self, url):
+        data = f'{{"cmd": "request.get","url": "{url}", "session": "{self.id}"}}'
+        return _request(data)['solution']
+
+    def post(self, url, post_data):
+        data_list = "&".join(f'{k}={v}' for k, v in post_data.items())
+        data = f'{{"cmd": "request.post","url": "{url}", "session": "{self.id}", "postData":"{data_list}"}}'
+        return _request(data)['solution']
